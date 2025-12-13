15a7955bce1250f6c056cfc2dbe7a05140e7c0b0
Guru <202317b2666@wilp.bits-pilani.ac.in>
2025-12-13 13:08:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,81 +1,38 @@
 import streamlit as st
-import pandas as pd
 import plotly.express as px
+import pandas as pd
 
-st.set_page_config(layout="wide")
-st.title("🌍 World Map – Pop-out on Click Simulation")
-
-# ---------------- DATA ----------------
+# Sample data
 df = pd.DataFrame({
-    "country": ["India", "United States", "Germany"],
-    "iso_alpha": ["IND", "USA", "DEU"],
-    "Life Expectancy": [69.7, 78.9, 81.1],
-    "HDI": [0.633, 0.921, 0.942]
+    "country": ["India", "United States", "China"],
+    "iso": ["IND", "USA", "CHN"],
+    "value": [1, 2, 3],
+    "img": [
+        "https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg",
+        "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg",
+        "https://upload.wikimedia.org/wikipedia/commons/0/0d/Flag_of_China.svg"
+    ]
 })
 
-# ---------------- STATE ----------------
-if "show_popup" not in st.session_state:
-    st.session_state.show_popup = False
-
-# ---------------- BASE MAP ----------------
-base_fig = px.choropleth(
+fig = px.choropleth(
     df,
-    locations="iso_alpha",
-    color="Life Expectancy",
+    locations="iso",
+    color="value",
     hover_name="country",
+    custom_data=["img"],
     color_continuous_scale="Blues"
 )
 
-base_fig.update_layout(
-    geo=dict(
-        projection_type="natural earth",
-        showcountries=True,
-        countrycolor="white"
-    ),
-    height=420,
+fig.update_traces(
+    hovertemplate="""
+    <b>%{hovertext}</b><br><br>
+    <img src="%{customdata[0]}" width="120"><br>
+    """
+)
+
+fig.update_layout(
+    geo=dict(showframe=False, showcoastlines=False),
     margin=dict(l=0, r=0, t=0, b=0)
 )
 
-st.subheader("Base Map")
-st.plotly_chart(base_fig, use_container_width=True)
-
-# ---------------- TRIGGER ----------------
-st.markdown("### 🔘 Click to Pop-out Map")
-col1, col2, col3 = st.columns(3)
-
-if col1.button("🇮🇳 India"):
-    st.session_state.show_popup = True
-
-if col2.button("🇺🇸 USA"):
-    st.session_state.show_popup = True
-
-if col3.button("🇩🇪 Germany"):
-    st.session_state.show_popup = True
-
-# ---------------- POP-OUT MAP ----------------
-if st.session_state.show_popup:
-    st.markdown("---")
-    st.subheader("🔍 Focus View (Pop-out)")
-
-    popup_fig = px.choropleth(
-        df,
-        locations="iso_alpha",
-        color="Life Expectancy",
-        hover_name="country",
-        color_continuous_scale="Blues"
-    )
-
-    popup_fig.update_layout(
-        geo=dict(
-            projection_type="natural earth",
-            showcountries=True,
-            countrycolor="black"
-        ),
-        height=600,   # 👈 bigger = pop-out feel
-        margin=dict(l=0, r=0, t=0, b=0)
-    )
-
-    st.plotly_chart(popup_fig, use_container_width=True)
-
-    if st.button("❌ Close"):
-        st.session_state.show_popup = False
+st.plotly_chart(fig, use_container_width=True)
